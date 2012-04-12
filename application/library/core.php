7f16cf2d4eeb1d8c991fe568832f154d91e41966
Roger Dudler <roger.dudler@gmail.com>
2012-04-12 17:27:34 +0200

    added uuid function.

--- application/library/core.php
+++ application/library/core.php
@@ -88,4 +88,15 @@ function truncate($string, $length, $add = '...') {
     return $string;
 }
 
+function gen_uuid($salt, $len = 8) {
+    $hex = md5($salt . uniqid("", true));
+    $pack = pack('H*', $hex);
+    $tmp =  base64_encode($pack);
+    $uid = preg_replace("#(*UTF8)[^A-Za-z0-9]#", "", $tmp);
+    $len = max(4, min(128, $len));
+    while (strlen($uid) < $len)
+        $uid .= gen_uuid(22);
+    return substr($uid, 0, $len);
+}
+
 ?>
