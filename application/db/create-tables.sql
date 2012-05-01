f026699297d86b51aecd794f4c3645cff9b85080
Remo Brunschwiler <remo.brunschwiler@namics.com>
2012-05-01 10:42:19 +0200

    switch back to old create tables sql

--- application/db/create-tables.sql
+++ application/db/create-tables.sql
@@ -1,25 +1,30 @@
--- phpMyAdmin SQL Dump
--- version 3.3.9.2
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Apr 27, 2012 at 10:09 PM
--- Server version: 5.5.9
--- PHP Version: 5.3.6
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 3408
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.5.20)
+# Database: clarify
+# Generation Time: 2012-03-14 11:34:07 +0000
+# ************************************************************
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
---
--- Database: `clarify`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `behaviour`
---
+# Dump of table behaviour
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `behaviour`;
+
 CREATE TABLE `behaviour` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `creator` int(11) DEFAULT NULL,
@@ -31,15 +36,15 @@ CREATE TABLE `behaviour` (
   `x` int(11) DEFAULT NULL,
   `y` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `behaviour_event`
---
+
+# Dump of table behaviour_event
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `behaviour_event`;
+
 CREATE TABLE `behaviour_event` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `creator` int(11) NOT NULL,
@@ -50,15 +55,15 @@ CREATE TABLE `behaviour_event` (
   `event` int(11) NOT NULL,
   `description` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `behaviour_option`
---
+
+# Dump of table behaviour_option
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `behaviour_option`;
+
 CREATE TABLE `behaviour_option` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `creator` int(11) NOT NULL,
@@ -69,27 +74,27 @@ CREATE TABLE `behaviour_option` (
   `option` int(11) NOT NULL,
   `value` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `changelog`
---
+
+# Dump of table changelog
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `changelog`;
+
 CREATE TABLE `changelog` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `color`
---
+
+# Dump of table color
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `color`;
+
 CREATE TABLE `color` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `creator` int(11) DEFAULT NULL,
@@ -102,15 +107,15 @@ CREATE TABLE `color` (
   `y` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `screen` (`screen`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=62 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `comment`
---
+
+# Dump of table comment
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `comment`;
+
 CREATE TABLE `comment` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `creator` int(11) NOT NULL,
@@ -127,15 +132,15 @@ CREATE TABLE `comment` (
   `content` text,
   PRIMARY KEY (`id`),
   KEY `screen` (`screen`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `font`
---
+
+# Dump of table font
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `font`;
+
 CREATE TABLE `font` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `creator` int(11) NOT NULL,
@@ -148,207 +153,13 @@ CREATE TABLE `font` (
   `y` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `screen` (`screen`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `library_behaviour`
---
-
-DROP TABLE IF EXISTS `library_behaviour`;
-CREATE TABLE `library_behaviour` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `creator` int(11) NOT NULL,
-  `created` datetime NOT NULL,
-  `modifier` int(11) DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  `name` varchar(40) DEFAULT NULL,
-  `vendor` varchar(40) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `library_behaviour_event`
---
-
-DROP TABLE IF EXISTS `library_behaviour_event`;
-CREATE TABLE `library_behaviour_event` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `creator` int(1) DEFAULT NULL,
-  `created` datetime DEFAULT NULL,
-  `modifier` int(11) DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  `behaviour` int(11) DEFAULT NULL,
-  `name` varchar(40) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `library_behaviour_option`
---
-
-DROP TABLE IF EXISTS `library_behaviour_option`;
-CREATE TABLE `library_behaviour_option` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `creator` int(11) NOT NULL,
-  `created` datetime NOT NULL,
-  `modifier` int(11) DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  `behaviour` int(11) NOT NULL,
-  `name` varchar(40) NOT NULL DEFAULT '',
-  `value_type` varchar(40) NOT NULL DEFAULT '',
-  `value_default` varchar(40) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `library_component`
---
-
-DROP TABLE IF EXISTS `library_component`;
-CREATE TABLE `library_component` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `creator` int(11) DEFAULT NULL,
-  `created` datetime DEFAULT NULL,
-  `modifier` int(11) DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `vendor` varchar(100) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `library_component_behaviour`
---
-
-DROP TABLE IF EXISTS `library_component_behaviour`;
-CREATE TABLE `library_component_behaviour` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `creator` int(11) DEFAULT NULL,
-  `created` datetime DEFAULT NULL,
-  `modifier` int(11) DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  `component` int(11) DEFAULT NULL,
-  `behaviour` int(11) DEFAULT NULL,
-  `state` enum('REQUESTED','DEVELOPMENT','READY') DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `measure`
---
-
-DROP TABLE IF EXISTS `measure`;
-CREATE TABLE `measure` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `creator` int(11) NOT NULL,
-  `created` datetime NOT NULL,
-  `modifier` int(11) DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  `screen` int(11) DEFAULT NULL,
-  `type` enum('SINGLE','SIZE','ELEMENT') DEFAULT 'SINGLE',
-  `x` int(11) NOT NULL,
-  `y` int(11) NOT NULL,
-  `width` int(11) DEFAULT NULL,
-  `height` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `screen` (`screen`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `module`
---
-
-DROP TABLE IF EXISTS `module`;
-CREATE TABLE `module` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `creator` int(11) NOT NULL,
-  `created` datetime NOT NULL,
-  `modifier` int(11) DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  `screen` int(11) DEFAULT NULL,
-  `module` int(11) DEFAULT NULL,
-  `x` int(11) NOT NULL,
-  `y` int(11) NOT NULL,
-  `width` int(11) DEFAULT NULL,
-  `height` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `screen` (`screen`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=328 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `project`
---
-
-DROP TABLE IF EXISTS `project`;
-CREATE TABLE `project` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `creator` int(11) NOT NULL,
-  `created` datetime NOT NULL,
-  `modifier` int(11) DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `screen_count` int(11) NOT NULL DEFAULT '0',
-  `slug` varchar(40) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `creator` (`creator`,`slug`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `project_color`
---
-
-DROP TABLE IF EXISTS `project_color`;
-CREATE TABLE `project_color` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `creator` int(11) DEFAULT NULL,
-  `created` datetime NOT NULL,
-  `modifier` int(11) DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  `project` int(11) NOT NULL,
-  `reference_color` int(11) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `name_css` varchar(255) DEFAULT NULL,
-  `r` int(4) DEFAULT '0',
-  `g` int(4) DEFAULT '0',
-  `b` int(4) DEFAULT '0',
-  `hex` varchar(6) DEFAULT NULL,
-  `alpha` int(4) DEFAULT '0',
-  `hue` int(4) NOT NULL DEFAULT '0',
-  `saturation` int(4) NOT NULL DEFAULT '0',
-  `lightness` int(4) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `project` (`project`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=39 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `project_font`
---
+# Dump of table project_font
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `project_font`;
+
 CREATE TABLE `project_font` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `creator` int(11) NOT NULL,
@@ -383,35 +194,184 @@ CREATE TABLE `project_font` (
   `decoration_visited` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `project` (`project`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `project_module`
---
 
-DROP TABLE IF EXISTS `project_module`;
-CREATE TABLE `project_module` (
+# Dump of table library_behaviour
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `library_behaviour`;
+
+CREATE TABLE `library_behaviour` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `creator` int(11) NOT NULL,
   `created` datetime NOT NULL,
   `modifier` int(11) DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `vendor` varchar(40) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table library_behaviour_event
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `library_behaviour_event`;
+
+CREATE TABLE `library_behaviour_event` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `creator` int(1) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modifier` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `behaviour` int(11) DEFAULT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table library_behaviour_option
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `library_behaviour_option`;
+
+CREATE TABLE `library_behaviour_option` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `creator` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modifier` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `behaviour` int(11) NOT NULL,
+  `name` varchar(40) NOT NULL DEFAULT '',
+  `value_type` varchar(40) NOT NULL DEFAULT '',
+  `value_default` varchar(40) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table library_component
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `library_component`;
+
+CREATE TABLE `library_component` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `creator` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modifier` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `vendor` varchar(100) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table library_component_behaviour
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `library_component_behaviour`;
+
+CREATE TABLE `library_component_behaviour` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `creator` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modifier` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `component` int(11) DEFAULT NULL,
+  `behaviour` int(11) DEFAULT NULL,
+  `state` enum('REQUESTED','DEVELOPMENT','READY') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table measure
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `measure`;
+
+CREATE TABLE `measure` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `creator` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modifier` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `screen` int(11) DEFAULT NULL,
+  `type` enum('SINGLE','SIZE','ELEMENT') DEFAULT 'SINGLE',
+  `x` int(11) NOT NULL,
+  `y` int(11) NOT NULL,
+  `width` int(11) DEFAULT NULL,
+  `height` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `screen` (`screen`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table project
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `project`;
+
+CREATE TABLE `project` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `creator` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modifier` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `screen_count`int(11) DEFAULT '0' NOT NULL,
+  `slug` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX (`creator`, `slug`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table project_color
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `project_color`;
+
+CREATE TABLE `project_color` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `creator` int(11) DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modifier` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
   `project` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `skin` varchar(255) NOT NULL,
+  `reference_color` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `name_css` varchar(255) DEFAULT NULL,
+  `r` int(4) DEFAULT '0',
+  `g` int(4) DEFAULT '0',
+  `b` int(4) DEFAULT '0',
+  `hex` varchar(6) DEFAULT NULL,
+  `alpha` int(4) DEFAULT '0',
+  `hue` int(4) NOT NULL DEFAULT '0',
+  `saturation` int(4) NOT NULL DEFAULT '0',
+  `brightness` int(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `project` (`project`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=248 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `screen`
---
+
+# Dump of table screen
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `screen`;
+
 CREATE TABLE `screen` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `creator` int(11) NOT NULL,
@@ -428,25 +388,18 @@ CREATE TABLE `screen` (
   `align_vertical` enum('TOP','CENTER','BOTTOM') DEFAULT 'TOP',
   `type` varchar(40) DEFAULT NULL,
   `ext` varchar(40) DEFAULT NULL,
-  `embeddable` enum('TRUE','FALSE') DEFAULT NULL,
-  `count_comment` int(11) NOT NULL DEFAULT '0',
-  `count_measure` int(11) NOT NULL DEFAULT '0',
-  `count_color` int(11) NOT NULL DEFAULT '0',
-  `count_font` int(11) NOT NULL DEFAULT '0',
-  `count_module` int(11) NOT NULL DEFAULT '0',
+  `embeddable` ENUM('TRUE','FALSE') NULL DEFAULT NULL,
+  `count_comment` INT(11) NOT NULL DEFAULT '0',
+  `count_measure` INT(11) NOT NULL DEFAULT '0',
+  `count_color` INT(11) NOT NULL DEFAULT '0',
+  `count_font` INT(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `code` (`code`),
   KEY `project` (`project`),
-  KEY `project_2` (`project`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
+  UNIQUE KEY `code`(`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `user`;
+
 CREATE TABLE `user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `creator` int(11) DEFAULT NULL,
@@ -457,10 +410,18 @@ CREATE TABLE `user` (
   `twitter_screen_name` varchar(40) DEFAULT NULL,
   `twitter_oauth_token` varchar(100) DEFAULT NULL,
   `twitter_oauth_secret` varchar(100) DEFAULT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `invitation_code` varchar(10) DEFAULT NULL,
+  `name` VARCHAR(100) NULL DEFAULT NULL,
+  `invitation_code` VARCHAR(10) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`),
   UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `twitter_user_id` (`twitter_user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  UNIQUE KEY `twitter_user_id` (`twitter_user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
