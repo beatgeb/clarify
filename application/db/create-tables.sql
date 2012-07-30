ef540eea91d2230258d5f1e1ad521e9495d166a4
Roger Dudler <roger.dudler@gmail.com>
2012-07-30 22:17:17 +0200

    added module enum value to object types.

--- application/db/create-tables.sql
+++ application/db/create-tables.sql
@@ -475,10 +475,10 @@ CREATE TABLE `activity` (
   `creator` int(11) NOT NULL,
   `created` datetime NOT NULL,
   `actor` int(11) NOT NULL,
-  `actor_type` enum('PROJECT','SCREEN','USER','MEASURE','COLOR','FONT','COMMENT') NOT NULL DEFAULT 'PROJECT',
+  `actor_type` enum('PROJECT','SCREEN','USER','MEASURE','COLOR','FONT','COMMENT','MODULE') NOT NULL DEFAULT 'PROJECT',
   `verb` enum('NONE','COMMENT','MEASURE','PICK','DEFINE','JOIN','CREATE','ADD') NOT NULL DEFAULT 'NONE',
   `object` int(11) NOT NULL,
-  `object_type` enum('PROJECT','SCREEN','USER','MEASURE','COLOR','FONT','COMMENT') NOT NULL DEFAULT 'PROJECT',
+  `object_type` enum('PROJECT','SCREEN','USER','MEASURE','COLOR','FONT','COMMENT','MODULE') NOT NULL DEFAULT 'PROJECT',
   PRIMARY KEY (`id`),
   KEY `actor` (`actor`,`actor_type`),
   KEY `object` (`object`,`object_type`)
