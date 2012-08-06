be48733a27c1671b584db62b1bc9064458138c04
Roger Dudler <roger.dudler@gmail.com>
2012-08-06 19:59:43 +0200

    fixed #46 - Export Color Library to SASS/SCSS.

--- public/modules/ProjectBrowser/js/Tc.Module.ProjectBrowser.js
+++ public/modules/ProjectBrowser/js/Tc.Module.ProjectBrowser.js
@@ -9,8 +9,11 @@
 (function($) {
     Tc.Module.ProjectBrowser = Tc.Module.extend({
         onBinding: function() {
+
             var $ctx = this.$ctx;
             var that = this;
+            var $modal = $('.modal-confirm');
+
             $('.btn-add-project', $ctx).on('click', function() {
                 $('.add-project', $ctx).slideToggle('fast', function() {
                     $('.project-name', $ctx).focus();
@@ -49,6 +52,50 @@
             $('.edit-collaborator', this.$ctx).on('click', function() {
                 $('.popbox').popbox();
                 return false;
+            });
+
+            $('.btn-export-css').on('click', function(e) {
+                var $colors = $('.colors a', $ctx);
+                var colors = '';
+                $colors.each(function(){
+                     colors += $(this).data('less')+"\r\n"; 
+                });
+
+                $('h3', $modal).text('Copy & Paste the following LESS Template');
+                $('p', $modal).empty();
+
+                var code = $('<pre></pre>').text(colors);
+                $('p', $modal).append($('<span>Here are all your colors that you have specified for this project:</span>'));
+                $('p', $modal).append(code);
+                $('.btn-confirm', $modal).text('Close');
+                $('.btn-confirm', $modal).on('click', function() {
+                    $modal.modal('hide');
+                    e.stopPropagation();
+                    return false;
+                });
+                $modal.modal();
+            });
+
+            $('.btn-export-sass').on('click', function(e) {
+                var $colors = $('.colors a', $ctx);
+                var colors = '';
+                $colors.each(function(){
+                     colors += $(this).data('sass')+"\r\n"; 
+                });
+
+                $('h3', $modal).text('Copy & Paste the following Sass Template');
+                $('p', $modal).empty();
+
+                var code = $('<pre></pre>').text(colors);
+                $('p', $modal).append($('<span>Here are all your colors that you have specified for this project:</span>'));
+                $('p', $modal).append(code);
+                $('.btn-confirm', $modal).text('Close');
+                $('.btn-confirm', $modal).on('click', function() {
+                    $modal.modal('hide');
+                    e.stopPropagation();
+                    return false;
+                });
+                $modal.modal();
             });
 
             $('.color', this.$ctx).tooltip();
