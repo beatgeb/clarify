f13e988213599a541a62a454a9dc67284aa7778d
Roger Dudler <roger.dudler@gmail.com>
2012-08-10 13:41:50 +0200

    updated styling of styleguide.

--- application/views/guide.php
+++ application/views/guide.php
@@ -36,7 +36,7 @@ foreach ($comments as $comment) {
 <body>
     <div class="mod modGuide">
         <div class="mod modToolbar">
-            <a class="button btn-screen" href="/project/<?= $project['creator']; ?>/<?= $project['slug']; ?>/"><i class="icon-white icon-chevron-left"></i> Back</a>
+            <a class="button btn-screen" href="/project/<?= $project['creator']; ?>/<?= $project['slug']; ?>/"><i class="icon-chevron-left"></i> Back</a>
         </div>
         <h1><?= $project['name'] ?></h1>
         <div class="chapter">
