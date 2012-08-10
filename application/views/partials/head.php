8b4b19bb401fdc70be0044b53150a08c6ca06e11
Roger Dudler <roger.dudler@gmail.com>
2012-08-10 13:31:50 +0200

    updated toolbar styling.

--- application/views/partials/head.php
+++ application/views/partials/head.php
@@ -1,2 +1,3 @@
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="<?= R ?>css/css.php" />
+<link type="text/css" rel="stylesheet" href="http://fast.fonts.com/cssapi/9803a5bf-fbdf-44f6-a33f-29a977572db6.css"/>
