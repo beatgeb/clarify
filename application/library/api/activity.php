e4420e4698a108666c0181c156f0dac0f0f27b9e
Roger Dudler <roger.dudler@gmail.com>
2012-08-10 15:02:31 +0200

    added user activity stream.

--- application/library/api/activity.php
+++ application/library/api/activity.php
@@ -10,6 +10,21 @@ switch ($action) {
 	case API_ACTIVITY_STREAM:
 		$stream = $route[4];
 		switch ($stream) {
+			case OBJECT_TYPE_USER:
+				$id = intval($route[5]);
+				if (userid() != $id) { die(); }
+				$stream = "stream." . $stream . "." . $id;
+				$activity_ids = $cache->get($stream);
+				$activities = array();
+				if (is_array($activity_ids)) {
+					foreach($activity_ids as $id) {
+						$activity = $cache->get('activity.' . $id);
+						$activities[] = $activity;
+					}
+				}
+				header('Content-Type: application/json');
+				echo json_encode(array('items' => $activities));
+				break;
 			case OBJECT_TYPE_PROJECT:
 				$id = intval($route[5]);
 				permission($id, 'VIEW');
