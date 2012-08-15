1101e6ba96dc876709e1540872b7bbd680876d0b
Roger Dudler <roger.dudler@gmail.com>
2012-08-15 12:19:58 +0200

    moved to https.

--- application/views/partials/head.php
+++ application/views/partials/head.php
@@ -1,4 +1,4 @@
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="<?= R ?>css/css.php" />
-<link type="text/css" rel="stylesheet" href="http://fast.fonts.com/cssapi/9803a5bf-fbdf-44f6-a33f-29a977572db6.css"/>
+<link type="text/css" rel="stylesheet" href="https://fast.fonts.com/cssapi/9803a5bf-fbdf-44f6-a33f-29a977572db6.css"/>
 <link rel="icon" href="<?= R ?>img/favicon.png" type="image/x-icon" />
