7d0343fdbcf49385b747b970a9f575b07cf8787b
Roger Dudler <roger.dudler@gmail.com>
2012-08-20 21:58:00 +0200

    added user voice.

--- application/views/partials/foot.php
+++ application/views/partials/foot.php
@@ -22,3 +22,11 @@
     });
 })(Tc.$);
 </script>
+<script type="text/javascript">
+  var uvOptions = {};
+  (function() {
+    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/u5QnuwlRcK4saGqrAKVZEA.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+  })();
+</script>
