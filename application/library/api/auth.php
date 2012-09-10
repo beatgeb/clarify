265d4d39107c5d102e796f0b410d5dcce866f40e
Roger Dudler <roger.dudler@gmail.com>
2012-09-10 22:37:34 +0200

    added authentication api.

--- application/library/api/auth.php
+++ application/library/api/auth.php
@@ -6,7 +6,23 @@ define('API_AUTH_AUTHENTICATE', 'auth.authenticate');
 switch ($action) {
 
 	case API_AUTH_AUTHENTICATE:
-		// TODO
+		$email = $_REQUEST['email'];
+		$password = md5($_REQUEST['password'] . config('security.password.hash'));
+		$result = array('success' => false);
+
+		// validate authentification (only with valid email)
+		if (filter_var($_REQUEST['email'], FILTER_VALIDATE_EMAIL)) {
+			$user = $db->single("SELECT id, name FROM user WHERE email = '" . $email . "' AND password = '" . $password . "' LIMIT 1");
+			if ($user) {
+				$_SESSION['user']['id'] = $user['id'];
+		        $_SESSION['user']['name'] = $user['name'];
+		        $_SESSION['auth'] = md5(config('security.password.hash') . $_SESSION['user']['id']);
+				$result = array('success' => true);
+			}
+		}
+
+		header('Content-Type: application/json');
+		echo json_encode($result);
 		break;
 
 }
