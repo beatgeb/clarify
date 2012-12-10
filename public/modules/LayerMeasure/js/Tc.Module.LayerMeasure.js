8b29758b52d6536c1213675ccd8822bac71d4782
Roger Dudler <roger.dudler@gmail.com>
2012-12-10 20:17:20 +0100

    fix.

--- public/modules/LayerMeasure/js/Tc.Module.LayerMeasure.js
+++ public/modules/LayerMeasure/js/Tc.Module.LayerMeasure.js
@@ -190,6 +190,8 @@
                                     }
                                 });
                             }
+                            width = ui.size.width;
+                            height = ui.size.height;
                         }
                     });
                     that.resize = false;
