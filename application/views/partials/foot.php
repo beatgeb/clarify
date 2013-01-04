0d882226159c68ce14800429365fe316ec00ef95
Roger Dudler <roger.dudler@gmail.com>
2013-01-04 16:29:03 +0100

    added google analytics.

--- application/views/partials/foot.php
+++ application/views/partials/foot.php
@@ -38,3 +38,13 @@
   _kms('//doug1izaerwt3.cloudfront.net/' + _kmk + '.1.js');
   _kmq.push(['identify', '<?= user('email') ?>']);
 </script>
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-652147-17']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
